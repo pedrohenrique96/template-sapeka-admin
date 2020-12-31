fbbdbee0fcc511c680e4b60a5cdf2fa6e1de5600
C1313350 Pedro Henrique Carvalho Rodrigues <c1313350@interno.bb.com.br>
2020-12-31 03:31:26 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  return <div className="App" />;
+};
 
 export default App;
